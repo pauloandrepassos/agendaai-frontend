07b599893562dada3b476f363fe4bcc857fbe9d7
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-06-20 16:35:49 -0300

    Integração de cadastro de usuário

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -2,12 +2,57 @@
 
 import Navbar from "@/components/Navbar";
 import styles from '../auth.module.css'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { apiUrl } from "@/config/api";
+import Modal from "@/components/SucessErrorModal";
+import axios from "axios";
+
+const schema = yup.object().shape({
+    nome: yup.string().min(3, 'O nome de usuário deve ter pelo menos 3 dígitos').required('O nome deve vser preenchido'),
+    email: yup.string().email('Formato de email inválido').required('O email deve ser preenchido'),
+    password: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('A senha deve ser preenchida'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'As senhas devem ser correspondentes').required('A confirmação de senha deve ser preenchida')
+})
 
 export default function Signup() {
-    
+    const router = useRouter()
+
+    const [message, setMessage] = useState(null)
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onChange"
+    })
+
+    const onSubmit = (data) => {
+        //setLoading(true)
+        axios.post(`${apiUrl}/register`, {
+            nome: data.nome,
+            email: data.email,
+            password: data.password,
+            confirmPassword: data.confirmPassword
+        }).then((response) => {
+            setMessage('Foi enviado um link de confirmação para o seu email.')
+            //router.push('/auth/signupConfirm')
+        }).catch((error) => {
+            //setLoading(false)
+            if (error.response) {
+                const responseData = error.response.data;
+                if (responseData.error) {
+                    setMessage(responseData.error)
+                }
+            } else {
+                setMessage(error.message)
+            }
+        })
+    }
+
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -25,23 +70,26 @@ export default function Signup() {
             <section className={styles.cadastro}>
                 <div className={styles.cardCadastro}>
                     <h1>Cadastro</h1>
-                    <form className={styles.formCadastro}>
+                    <form onSubmit={handleSubmit(onSubmit)} className={styles.formCadastro}>
                         <input
                             type='text'
-                            id='nomeUsuario'
-                            name='nomeUsuario'
+                            id='nome'
+                            name='nome'
                             placeholder='Digite seu nome'
                             required
-                            //{...register("nomeUsuario")}
+                            {...register("nome")}
                         />
+                        {errors.nome && <p className={styles.mensagemDeErro}>{errors.nome.message}</p>}
                         <input
                             type='text'
                             id='email'
                             name='email'
                             placeholder='Digite seu email'
                             required
-                            //{...register("email")}
+                            {...register("email")}
                         />
+                        {errors.email && <p className={styles.mensagemDeErro}>{errors.email.message}</p>}
+
 
                         <div className={styles.inputSenha}>
                             <input
@@ -50,10 +98,12 @@ export default function Signup() {
                                 name='password'
                                 placeholder='Digite sua senha'
                                 required
-                                //{...register("password")}
+                                {...register("password")}
                             />
                             <FontAwesomeIcon className={styles.icon} icon={showPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('password')} />
                         </div>
+                        {errors.password && <p className={styles.mensagemDeErro}>{errors.password.message}</p>}
+
 
                         <div className={styles.inputSenha}>
                             <input
@@ -62,19 +112,29 @@ export default function Signup() {
                                 name='confirmPassword'
                                 placeholder='Confirme sua senha'
                                 required
-                                //{...register("confirmPassword")}
+                                {...register("confirmPassword")}
                             />
                             <FontAwesomeIcon className={styles.icon} icon={showConfirmPassword ? faEyeSlash : faEye} onClick={() => togglePasswordVisibility('confirmPassword')} />
                         </div>
+                        {errors.confirmPassword && <p className={styles.mensagemDeErro}>{errors.confirmPassword.message}</p>}
+
                         <button
                             id="buttonLogin"
                             className={styles.submitButton}
+                            type="submit"
                         >
                             Cadastrar
                         </button>
                     </form>
                 </div>
             </section>
+            {message &&
+                <Modal
+                    isOpen={true}
+                    onClose={() => setMessage(null)}
+                    message={message}
+                />
+            }
         </>
     )
 }
