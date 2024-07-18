c656a9fb91b0a21601603b392d5062c6faff0f2e
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-07-18 12:52:23 -0300

    verificação de token

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,54 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { decodeToken, isTokenValid } from "@/services/Token";
+import { useRouter } from "next/navigation";
+import Loading from "@/components/Loading";
 
 export default function Home() {
+  const [token, setToken] = useState('')
+  const router = useRouter()
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const storedToken = localStorage.getItem('token');
+      if (storedToken) {
+        const { isValid, status } = isTokenValid(storedToken);
+        if (isValid) {
+          const decodedToken = decodeToken(storedToken);
+          if (decodedToken) {
+            switch (decodedToken.papel) {
+              case "admin":
+                router.push("/admin");
+                break;
+              case "gerente":
+                router.push("/gerente");
+                break;
+              case "cliente":
+                router.push("/home");
+                break;
+              default:
+                router.push("/inicio");
+                break;
+            }
+          }
+        } else if (status === "expirado") {
+          router.push("/login");
+        } else {
+          router.push("/inicio");
+        }
+      } else {
+        router.push("/inicio");
+      }
+    };
+    fetchData();
+  }, [router]);
+
   return (
     <main className={styles.main}>
-      <Link href={'\inicio'}>Link para Página de apresentação</Link>
+      <Loading />
     </main>
   );
 }
