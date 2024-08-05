ee479756227e8d052794e7f332553ecb9d479eae
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-08-05 19:02:47 -0300

    página inicial

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -1,13 +1,27 @@
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
+import styes from './inicio.module.css'
 
 export default function Inicio() {
     return (
-        <div>
+        <div className={styes.inicial_page}>
             <Navbar />
-            <Link href={'/auth/signup'}>Cadastro</Link>
-            <Link href={'/auth/login'}>Login</Link>
-            <Link href={'/cadastrarLanchonete'}>Cadastrar Lanchonete</Link>
+            <div className={styes.section1}>
+                <div className={styes.center}>
+                    <h1>Agenda Aí</h1>
+                    <h2>Não espere pelo lanche,  faça ele esperar por você!!!</h2>
+                    <div className={styes.botoes}>
+                        <Link href={`/auth/login`}>Login</Link>
+                        <Link href={`/auth/signup`}>Cadastro</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styes.section2}>
+                <div className={styes.botoes2}>
+                    <Link href={`/cadastrarLanchonete`}>Cadastrar Lanchonete</Link>
+                </div>
+            </div>
+            
         </div>
     )
 }
