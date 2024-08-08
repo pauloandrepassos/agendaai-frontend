2b56ad8028446b4fcb1cc1b26ccbb4e71865f9b7
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-08-08 14:51:02 -0300

    Páginas listagem e adição de lanches

---
+++ src/app/(gerente)/lanchonete/[id]/lanche/page.js
@@ -0,0 +1,63 @@
+"use client"
+import { useEffect, useState } from 'react';
+import PrivateRouter from "@/components/PrivateRouter";
+import styles from './lanche.module.css';
+import Navbar from "@/components/Navbar";
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { apiUrl } from '@/config/api';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faSearch, faSliders, faStar } from '@fortawesome/free-solid-svg-icons';
+import CardLanche from '@/components/CardLanche';
+
+export default function Lanche() {
+    const router = useRouter();
+    const { id } = useParams();
+    const [lanches, setLanches] = useState([]);
+
+    useEffect(() => {
+        if (id) {
+            const fetchLanches = async () => {
+                try {
+                    const response = await axios.get(`${apiUrl}/lanchonete/${id}/lanche`);
+                    setLanches(response.data);
+                } catch (error) {
+                    console.error('Erro ao buscar lanches:', error);
+                }
+            };
+            fetchLanches();
+        }
+    }, [id]);
+
+    return (
+        <PrivateRouter tipoUsuario={'gerente'}>
+            <div className={styles.container}>
+                <Navbar />
+                <div className={styles.content}>
+                    <div className={styles.filters}>
+                        <div className={styles.searchContainer}>
+                            <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
+                            <input type="text" placeholder="Buscar lanche..." className={styles.searchBar} />
+                        </div>
+                        <Link href={`/lanchonete/${id}/adicionar-lanche`} className={styles.button}>
+                            Adicionar Lanche
+                            <FontAwesomeIcon icon={faPlus}/>
+                        </Link>
+                    </div>
+                    <div className={styles.lanchesContainer}>
+                        {lanches.map((lanche) => (
+                            <CardLanche
+                                id={lanche.id}
+                                nome={lanche.nome}
+                                imagem={lanche.imagem}
+                                preco={lanche.preco}
+                                url={`/`}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </PrivateRouter>
+    );
+}
---
+++ src/components/CardLanche.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from './CardLanche.module.css'
+
+export default function CardLanche({ id, nome, preco, imagem, url }) {
+    return (
+        <Link href={`/lanche/${id}`} key={id} className={styles.card}>
+            <div className={styles.cardImage}>
+                <img src={imagem} alt={nome} />
+            </div>
+            <div className={styles.divInfo}>
+                <h3>{nome}</h3>
+                <h2>R$ {preco.toFixed(2)}</h2>
+            </div>
+        </Link>
+    )
+}
