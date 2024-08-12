5782406fb604ff0f839ac31071cefbd10e3983da
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-08-12 10:24:15 -0300

    adicionando lanche ao cesto de compras

--- src/components/LancheModal.js
+++ src/components/LancheModal.js
@@ -1,10 +1,12 @@
 "use client"
+import axios from 'axios';
 import { useState } from 'react';
 import styles from './LancheModal.module.css';
-import Image from 'next/image';
+import { apiUrl } from '@/config/api';
 
 export default function LancheModal({ lanche, onClose }) {
     const [quantidade, setQuantidade] = useState(1);
+    const [adicionando, setAdicionando] = useState(false); // Estado para gerenciar o texto do botão
 
     const incrementarQuantidade = () => {
         setQuantidade(prev => prev + 1);
@@ -14,9 +16,37 @@ export default function LancheModal({ lanche, onClose }) {
         setQuantidade(prev => (prev > 1 ? prev - 1 : 1));
     };
 
-    const handleAdd = () => {
-        onAdd(lanche, quantidade);
-        onClose();
+    const handleAdd = async () => {
+        setAdicionando(true); // Muda o estado para "adicionando"
+
+        try {
+            const token = localStorage.getItem('token');
+            const idLanchonete = lanche.idLanchonete;
+
+            const response = await axios.post(`${apiUrl}/cesto/adicionar`, {
+                idLanchonete,
+                idLanche: lanche.id,
+                quantidade
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': token
+                }
+            });
+
+            if (response.status === 200) {
+                console.log('Lanche adicionado ao cesto:', response.data);
+            } else {
+                console.error('Erro ao adicionar lanche ao cesto:', response.data);
+            }
+        } catch (error) {
+            console.error('Erro na requisição:', error.message);
+        }
+
+        setTimeout(() => {
+            setAdicionando(false); // Reseta o estado do botão
+            onClose(); // Fecha a modal após o tempo definido
+        }, 2000); // Aguarda 2 segundos antes de fechar a modal
     };
 
     return (
@@ -36,7 +66,9 @@ export default function LancheModal({ lanche, onClose }) {
                             <span className={styles.quantity}>{quantidade}</span>
                             <button onClick={incrementarQuantidade} className={styles.quantityButton}>+</button>
                         </div>
-                        <button className={styles.botaoAdicionar} onClick={onClose}>Adicionar</button>
+                        <button className={styles.botaoAdicionar} onClick={handleAdd}>
+                            {adicionando ? 'Adicionando lanche ao cesto...' : 'Adicionar'}
+                        </button>
                     </div>
                 </div>
             </div>
