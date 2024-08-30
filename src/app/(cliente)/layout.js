40d8cdde67c15153a45f8efdf5f24b16b6eac294
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-08-30 12:05:49 -0300

    Corrigindo bug do toast duplicado

--- src/app/(cliente)/layout.js
+++ src/app/(cliente)/layout.js
@@ -2,14 +2,16 @@
 
 import { wsApiUrl } from '@/config/api';
 import React, { useEffect, useState } from 'react';
-import Toast from '@/components/Toast'; 
-import { CestoProvider, useCesto } from '@/context/CestoContext'; // Importa o contexto
+import { usePathname } from 'next/navigation';
+import Toast from '@/components/Toast';
+import { useCesto } from '@/context/CestoContext';
 
 export default function ClienteLayout({ children }) {
     const [showToast, setShowToast] = useState(false);
-    const [toastMessage, setToastMessage] = useState("");
-    const [toastType, setToastType] = useState("");
-    const { setCesto } = useCesto(); // Usar o contexto para atualizar o cesto
+    const [toastMessage, setToastMessage] = useState('');
+    const [toastType, setToastType] = useState('');
+    const { setCesto } = useCesto();
+    const pathname = usePathname();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -19,22 +21,25 @@ export default function ClienteLayout({ children }) {
             ws.onmessage = (event) => {
                 const message = JSON.parse(event.data);
                 if (message.type === 'pedidoRetirado') {
-                    setToastMessage("Seu pedido foi retirado!");
-                    setToastType("success");
+                    setToastMessage('Seu pedido foi retirado!');
+                    setToastType('success');
                     setShowToast(true);
 
                     setTimeout(() => {
                         setShowToast(false);
                     }, 5000);
                 } else if (message.type === 'cestoAtualizado') {
-                    setCesto(message.cesto); // Atualiza o cesto de compras
-                    setToastMessage("Lanche adicionado no cesto!");
-                    setToastType("success");
-                    setShowToast(true);
+                    setCesto(message.cesto);
 
-                    setTimeout(() => {
-                        setShowToast(false);
-                    }, 5000);
+                    if (!pathname.startsWith('/lanchonete/')) {
+                        setToastMessage('Lanche adicionado no cesto!');
+                        setToastType('success');
+                        setShowToast(true);
+
+                        setTimeout(() => {
+                            setShowToast(false);
+                        }, 5000);
+                    }
                 }
             };
 
@@ -42,12 +47,12 @@ export default function ClienteLayout({ children }) {
                 ws.close();
             };
         }
-    }, [setCesto]);
+    }, [setCesto, pathname]);
 
     return (
-            <div>
-                {children}
-                {showToast && <Toast type={toastType} message={toastMessage} />}
-            </div>
+        <div>
+            {children}
+            {showToast && <Toast type={toastType} message={toastMessage} />}
+        </div>
     );
 }
