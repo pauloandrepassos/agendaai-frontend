7dba5973f6820b4d11e7c1ac700f865d2c011b6d
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-12-04 17:18:46 -0300

    hover

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -6,7 +6,7 @@ interface SubmitButtonProps {
 export default function SubmitButton({ text, isLoading }: SubmitButtonProps) {
     return (
         <button type="submit" disabled={isLoading} className={`mt-2 w-full rounded-full text-xl text-center text-white p-2 
-            ${isLoading ? "bg-[#FF5800] opacity-50 cursor-not-allowed" : "bg-[#FF0000] hover:bg-[#ff0000aa]"}
+            ${isLoading ? "bg-[#FF5800] opacity-50 cursor-not-allowed" : "bg-[#FF0000] hover:bg-[#e60000]"}
         `}>
             {isLoading ? `Carregando...` : text}
         </button>
