e20a4c578f36a99b5fb31504c2a270a15456a222
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2025-01-24 17:41:35 -0300

    Check auth status on auth pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,22 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter()
+  useEffect(()=> {
+    const getToken = async () => {
+      const token = await localStorage.getItem("token")
+      if(token) {
+        router.push("/")
+      }
+    }
+    getToken()
+  })
   return (
     <div
     className="min-h-screen flex items-center justify-center bg-cover bg-center"
